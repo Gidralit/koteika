fe0593d4cea7b8442d84beed11fd2b134c391975
Gidralit <feodorkrutkin23@gmail.com>
2024-11-24 19:55:45 +0700

    docker-compose

---
+++ backend/routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MyNewController;
+use App\Http\Controllers\PostController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/page1', [MyNewController::class, 'index']);
+Route::get('/posts', [PostController::class, 'index']);
---
+++ frontend/src/pages/HomePage.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {useCounterStore} from "@/stores/counter.js";
+import {storeToRefs} from "pinia";
+
+const { increment } = useCounterStore()
+const { count, doubleCount } = storeToRefs(useCounterStore())
+</script>
+
+<template>
+  <div>
+    <p>{{count}}</p>
+    <p>{{doubleCount}}</p>
+    <button @click="increment">sdffs</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
