eee469be8d40e0b747c548a8073c7a3923f902ab
Gidralit <feodorkrutkin23@gmail.com>
2024-12-01 21:26:33 +0700

    add contacts api

--- koteika/backend/database/seeders/DatabaseSeeder.php
+++ koteika/backend/database/seeders/DatabaseSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Database\Seeders\HeadersTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,5 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this ->call(HeadersTableSeeder::class);
+        $this -> call(ContactsTableSeeder::class);
     }
 }
