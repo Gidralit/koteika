afa3074879d946285c32a5efc4db3323d42f942d
Gidralit <feodorkrutkin23@gmail.com>
2024-12-03 19:49:06 +0700

    small edit authorization

--- koteika/backend/app/Http/Controllers/AuthController.php
+++ koteika/backend/app/Http/Controllers/AuthController.php
@@ -51,7 +51,15 @@ class AuthController extends Controller
             $user = auth()->user();
             $token = $user->createToken('kotiki')->plainTextToken;
 
-            return response()->json(['token' => $token], 200);
+            return response()->json(
+                [
+                    'token' => $token, 
+                    'email' =>$user->email, 
+                    'name' => $user->name
+                ], 
+                    200, 
+                    ['Content-Type' => 'application/json; charset=utf-8'], 
+                    JSON_UNESCAPED_UNICODE);
         }
 
         return response()->json(['message' => 'Unauthorized'], 401);
