88170697812d40ea0809d403e5cfe55f68a4fe09
Gidralit <feodorkrutkin23@gmail.com>
2024-12-03 16:58:48 +0700

    test_authorization

--- koteika/backend/routes/api.php
+++ koteika/backend/routes/api.php
@@ -5,10 +5,16 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HeaderController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\RoomController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/headers', [HeaderController::class, 'index']);
 Route::get('/contacts', [ContactController::class, 'index']);
 Route::get('/rooms', [RoomController::class, 'index']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
+
 Route::get('/tests', function () {
     return response()->json(['message' => 'api is working']);
 });
