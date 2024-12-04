a80f37ac9e6c5c131baae68f0815fc1b8e55a67f
Nastya <anastasion0107@gmail.com>
2024-12-04 21:29:37 +0700

    заготовка для страницы регистрации

--- koteika/frontend/src/components/register/RegisterForm.vue
+++ koteika/frontend/src/components/register/RegisterForm.vue
@@ -3,7 +3,16 @@
 </script>
 
 <template>
-<h1>регистрациялдваораплд</h1>
+<section class="register">
+  <div class="register-block">
+    <h2 class="register-title">Регистрация</h2>
+    <form action="" class="register-form">
+      <label for="name">Имя</label>
+      <input type="text" id="name" placeholder="Введите имя" required>
+      <label for="email">E-mail</label>
+    </form>
+  </div>
+</section>
 </template>
 
 <style scoped lang="scss">
