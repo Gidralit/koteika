ba659bcd58135f8021a80a66641965a472385be9
HiaenmaNoya <komstamtimovn232@gmail.com>
2024-12-08 00:40:50 +0700

    Initial Commit

--- koteika/backend/app/Http/Controllers/RoomController.php
+++ koteika/backend/app/Http/Controllers/RoomController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Reservation;
+use App\Validators\ReservationRequest;
 use App\Models\Room;
 use App\Services\RoomService;
+use Illuminate\Support\Facades\Auth;
 
 class RoomController extends Controller
 {
@@ -35,6 +38,27 @@ class RoomController extends Controller
     public function show(int $id)// Показать один номер
     {
         //
+    }
+    public function reservationRoom(ReservationRequest $request, Room $room, $roomId)
+    {
+
+
+        // Проверка доступности номера
+        $room = Room::find($roomId);
+        if (!$room) {
+            return response()->json(['message' => 'Комната не найдена'], 404);
+        }
+
+        // Создание нового бронирования
+        Reservation::create(
+            [
+                'user_id' => Auth::id(),
+                'room_id' => $roomId,
+                'price' => $room->price,
+                'description' => $room->description,
+            ],
+        );
+        return response()->json(['message' => 'Комната успешно забронирована'], 201);
     }
 
     public function update(Request $request, string $id) // Изменить статус номера админом(использовать политику)
