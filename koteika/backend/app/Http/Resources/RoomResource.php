bf65dd439a72a41e2b9185d04aed32fae19fcec3
Gidralit <111922002+Gidralit@users.noreply.github.com>
2024-12-11 00:24:34 +0700

    Update RoomResource.php

--- koteika/backend/app/Http/Resources/RoomResource.php
+++ koteika/backend/app/Http/Resources/RoomResource.php
@@ -11,7 +11,7 @@ class RoomResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'area' => ($this->width)*($this->lenght),
+            'area' => $this->width * $this->lenght,
             'price' => $this->price,
             'show_on_homepage' => $this->show_on_homepage,
             'equipment' => EquipmentResource::collection($this->whenLoaded('equipment')),
