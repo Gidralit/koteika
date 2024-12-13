3c8105e317cd4c33eef542e139ec35fac8c84bb2
Gidralit <feodorkrutkin23@gmail.com>
2024-12-13 09:36:57 +0700

    small edites

--- koteika/backend/app/Http/Requests/RoomRequest.php
+++ koteika/backend/app/Http/Requests/RoomRequest.php
@@ -31,14 +31,25 @@ class RoomRequest extends FormRequest
 
     public function messages():array{
         return[
-           'name.required' => 'Название номера обязательно для заполнения',
-           'name.regex' => 'Используйте кириллицу',
-           'square.required' => 'Поле обязательно для заполнения',
-           'square.integer' => 'Используйте число',
-           'price.required' => 'Поле обязательно для заполнения',
-           'price.integer' => 'Используйте целое число',
-           'equipment.array' => 'Должен содержать массив',
-           'show_on_homepage.boolean' => 'Используйте true или false',
+            'name.required' => 'Название номера обязательно для заполнения',
+            'name.regex' => 'Используйте кириллицу',
+            'square.required' => 'Поле обязательно для заполнения',
+            'square.integer' => 'Используйте число',
+            'price.required' => 'Поле обязательно для заполнения',
+            'price.integer' => 'Используйте целое число',
+            'equipment.array' => 'Должен содержать массив',
+            'show_on_homepage.boolean' => 'Используйте true или false',
+            'photo_path1.max' => '1 фотография превышает 2 мб',
+            'photo_path2.max' => '2 фотография превышает 2 мб',
+            'photo_path3.max' => '3 фотография превышает 2 мб',
+            'photo_path4.max' => '4 фотография превышает 2 мб',
+            'photo_path5.max' => '5 фотография превышает 2 мб',
+            'photo_path1.mimes' => '1 фотография имеет неверный формат, используйте jpeg или png',
+            'photo_path2.mimes' => '2 фотография имеет неверный формат, используйте jpeg или png',
+            'photo_path3.mimes' => '3 фотография имеет неверный формат, используйте jpeg или png',
+            'photo_path4.mimes' => '4 фотография имеет неверный формат, используйте jpeg или png',
+            'photo_path5.mimes' => '5 фотография имеет неверный формат, используйте jpeg или png',
+
         ];
     }
 
