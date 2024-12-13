7edecf7863a58fe6f853e10f2de4890c5cf2ab9e
HiaenmaNoya <komstamtimovn232@gmail.com>
2024-12-13 23:26:01 +0700

    Initial Commit

--- koteika/backend/routes/api.php
+++ koteika/backend/routes/api.php
@@ -11,12 +11,31 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\EquipmentController;
 
 
+    Route::get('/rooms/random', [RoomController::class, 'randomRooms']);
+
+    Route::get('/contacts', [ContactController::class, 'index']);
+    Route::get('/headers', [HeaderController::class, 'index']);
+
+    Route::get('/reviews/random', [ReviewController::class, 'randomCountReviews']);
+
+    Route::get('/rooms', [RoomController::class, 'index']);
+    Route::get('/rooms/{room}', [RoomController::class, 'show']);
+
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+
+    Route::middleware('auth:api')->apiResource('/user', UserController::class);
+    Route::get('/filters_data', [RoomController::class, 'dataForFilters']);
+//Доступно всем
+
+
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('/equipments', EquipmentController::class);
     Route::apiResource('/rooms', RoomController::class);
     Route::patch('/contacts', [ContactController::class, 'update']);
     Route::patch('/headers/update', [HeaderController::class, 'update']);
     Route::patch('/rooms/{room}/status', [RoomController::class, 'updateShowOnHomepage']);
+    Route::post('/reviews', [ReviewController::class, 'store']);
     Route::get('/reservation', [ReservationController::class, 'index']);
     Route::post('/reservation/{room}', [ReservationController::class, 'reservationRoom']);
     Route::delete('/cancelReservation/{id}', [ReservationController::class, 'cancelReservation']);
@@ -24,17 +43,6 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/reservation/{id}/approve', [ReservationController::class, 'approveReservation']);
     Route::get('/reservation/userReservations', [ReservationController::class, 'userReservations']);
 }); //Доступно авторизованным пользователям
-
-    Route::get('/equipments', [EquipmentController::class, 'index']);
-    Route::get('/contacts', [ContactController::class, 'index']);
-    Route::get('/headers', [HeaderController::class, 'index']);
-    Route::get('/rooms', [RoomController::class, 'index']);
-    Route::get('/rooms/{room}', [RoomController::class, 'show']);
-    Route::get('/reviews/random', [ReviewController::class, 'randomCountReviews']);
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::middleware('auth:api')->apiResource('/user', UserController::class);
-    //Доступно всем пользователям
 
 Route::prefix('api-reservation')->middleware(['auth:sanctum'])->group(function () {
 });
